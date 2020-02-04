251d32943e5cf20866157be264b4942f7c3a2fff
7rburden3 <ralphburden@hotmail.com>
2020-02-04 11:29:39 +0000

    add commenting exercise and first coding exercise

---
+++ comments.py
@@ -0,0 +1,11 @@
+print("Good morning")
+#print("Good afternoon")
+print("Good night")
+
+# this will perform a mathematical calculation
+print(1 + 1) # adds together 1 + 1
+
+# print(1 + 1)
+# print(1 + 1)
+# print(1 + 1)
+print("I am a developer!")
