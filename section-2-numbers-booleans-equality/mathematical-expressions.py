dc5953f6b1715fcb5f1f2ef3687345279ee6d180
7rburden3 <ralphburden@hotmail.com>
2020-02-05 10:01:53 +0000

    add basic mathematical calculations

---
+++ section-2-numbers-booleans-equality/mathematical-expressions.py
@@ -0,0 +1,15 @@
+print(3 + 4) # integers
+print(3.5 + 6.3) # floats
+print(10 - 5)
+print (5 * 3)
+
+print("Ba" + "con") # concatenation
+print("lolo" * 32)
+
+print(10 + 3.8) # python converts integer to more complex float
+
+print(5 ** 3) # exponential eg 5 to the power of 3
+print(4 ** 4)
+
+print(3 + 5 * 3) # pemdas example
+print((3 + 5) * 3) # pemdas example
