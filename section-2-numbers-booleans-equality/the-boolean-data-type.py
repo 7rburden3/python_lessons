b6ef7b859ba16914a54543552e799bd74fb2b9a4
7rburden3 <ralphburden@hotmail.com>
2020-02-25 16:48:08 +0000

    boolean and updated notes

---
+++ section-2-numbers-booleans-equality/the-boolean-data-type.py
@@ -0,0 +1,33 @@
+print(True)
+print(False)
+print("True") #true
+print("False") #false
+
+print(5 == 5) #true
+print(5 == 8) #false
+print(8.3 == 8.3) #true
+print(8.3 == 4.1) #false
+print(5 == "5") #false
+print(5 == 5.0) #true
+print(5 == 5.1) #false
+
+print("ham" == "ham") #true
+print("ham" == "bacon") #false
+print("ham" == "Ham") #false
+print("5" == "5") #true
+print("" == "") #true
+print("!==*" == "!==*") #true
+
+print(True == True) #true
+print(False == False) #true
+print(True == False) #false
+
+print(10 != 8) #true
+print(10 != 10) #false
+print("music" != "music") #false
+print("music" != "noise") #true
+print("music" != "Music") #true
+
+print(10 != "10") #true
+print(8.3 != 9.8) #true
+print(3.0 != 3) #false
