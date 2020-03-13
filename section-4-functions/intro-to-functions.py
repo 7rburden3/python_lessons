3157496df226bb20a535bbcb36b88e1de82a2059
7rburden3 <ralphburden@hotmail.com>
2020-03-13 11:20:15 +0000

    updated notes and start of functions lessons

---
+++ section-4-functions/intro-to-functions.py
@@ -0,0 +1,14 @@
+def output_text(): 
+    # start of the body or block of the function
+    print("Welcome to the progmam!")
+    print("It's nice to meet you.")
+    print("Have a wonderful day programming")
+    # end of the body or block of the function
+
+# so far we have declared the function. If we run it now, nothing will happen. We need to call it or invoke the function now for it to run.
+
+output_text()
+# this is calling or invoking the function
+
+output_text()
+# this is us reusing the function without having to write out all the three print statements again.
