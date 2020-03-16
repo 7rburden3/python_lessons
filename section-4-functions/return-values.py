f66f02818629c6ba633dca9fe94c44c02af21101
7rburden3 <ralphburden@hotmail.com>
2020-03-16 11:11:40 +0000

    updated notes with return values and complete coding assignment basics of functions

---
+++ section-4-functions/return-values.py
@@ -0,0 +1,10 @@
+def add(a, b):
+    # print(a + b)
+    return a + b
+    # return is a keyword that specifies what we are going to give as output
+
+    print("This is nonsense")
+    # This will not print as it is after the RETURN keyword and python will not reach it as it will step back outside the function after RETURN
+
+result = add(3, 5)
+print(result)
