298aec22ddad207e501d8ca2c315dd49b90936bf
7rburden3 <ralphburden@hotmail.com>
2020-03-16 11:27:32 +0000

    updated answers for basics of functions and add default arguments

--- coding-assignments/the-basics-of-functions.py
+++ coding-assignments/the-basics-of-functions.py
@@ -3,17 +3,18 @@
 def easy_money():
     return 100
 
-money = easy_money()
-print(money)
-
+# money = easy_money()
+# print(money)
+print(easy_money())
 
 # Define a best_food_ever function that accepts 
 # no parameters and always returns the string “Sushi”.
 def best_food_ever():
     return "Sushi"
 
-food = best_food_ever()
-print(food)
+# food = best_food_ever()
+# print(food)
+print(best_food_ever())
 
 # Define a convert_to_currency function that accepts a single parameter (an integer). 
 # The function should convert the argument to a string, prefix it with a dollar sign, and return the result.
@@ -21,5 +22,6 @@ print(food)
 def convert_to_currency(a):
     return "$" + str(a)
 
-currency = convert_to_currency(15)
-print(currency)
+# currency = convert_to_currency(15)
+# print(currency)
+print(convert_to_currency(15))
