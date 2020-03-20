5abd2a166f9067811f38ed539d4029b4e20791d5
7rburden3 <ralphburden@hotmail.com>
2020-03-20 10:42:41 +0000

    updated notes and add string length concatenation and immutability

---
+++ section-5-strings-the-basics/length-concatenation-and-immutability.py
@@ -0,0 +1,18 @@
+print(len("Python")) # returns 6
+print(len("programming")) # returns 11
+print(len("!@#$")) # returns 4
+print(len(" ")) # returns 1
+print(len("")) # returns 0
+
+# print(len(4)) will return a TypeError as it is an int and not a string
+
+# STRING CONCATENATION
+
+print("Ralph" + "Burden")
+print("Ralph " + "Burden")
+print("Ralph" + " Burden")
+print("Ralph" + " " + "Burden")
+
+print("a" "b" "c") # this works for concatenation but is bad practice
+
+print("---" * 30)
